8af3ad5d3a2b9339af05092df11a83da7e1fc3c9
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-12-05 16:30:47 +0100

    add network class

--- main.py
+++ main.py
@@ -48,3 +48,38 @@ class Node:
         print()
 
 
+class RoutingNetwork:
+    def __init__(self):
+        self.nodes = {}
+
+    def add_node(self, node):
+        """Add a node to the network"""
+        self.nodes[node.name] = node
+
+    def connect_nodes(self, node1, node2, distance):
+        """Connect two nodes with a specific distance"""
+        node1.add_neighbor(node2, distance)
+        node2.add_neighbor(node1, distance)
+
+    def run_routing_protocol(self, iterations=5):
+        """Simulate routing protocol updates"""
+        print("Running Distance Vector Routing Simulation\n")
+        
+        # Initial routing tables
+        for node in self.nodes.values():
+            node.print_routing_table()
+
+        # Perform routing updates
+        for i in range(iterations):
+            print(f"\nIteration {i + 1}:")
+            updated_nodes = []
+            
+            # Collect nodes that updated their routing tables
+            for node in self.nodes.values():
+                if node.update_routing_table():
+                    updated_nodes.append(node.name)
+            
+            # Print updated routing tables
+            for node_name in updated_nodes:
+                self.nodes[node_name].print_routing_table()
+
