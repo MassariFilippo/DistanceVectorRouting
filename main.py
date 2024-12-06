7104f0da91ccf029c7facaa0ed04975d7a25a464
Filippo Massari <filippo.massari6@studio.unibo.it>
2024-12-06 11:05:37 +0100

    second implementation of routering update

--- main.py
+++ main.py
@@ -1,87 +1,70 @@
-import math
-import copy
+iterations = 5
 
 class Node:
     def __init__(self, name):
         self.name = name
-        self.routing_table = {}
+        self.distanceVector = {}
+        self.distanceVector[self.name] = 0
         self.neighbors = {}
 
     def add_neighbor(self, neighbor, distance):
-        """Add a neighboring node with its direct distance"""
         self.neighbors[neighbor.name] = {
             'node': neighbor,
             'distance': distance
         }
-        # Initialize routing table entry for this neighbor
-        self.routing_table[neighbor.name] = distance
+        self.distanceVector[neighbor.name] = distance
 
-    def update_routing_table(self):
-        """Perform Distance Vector Routing algorithm update"""
+    def update_distanceVector(self):
         updated = False
-        for dest, current_dist in list(self.routing_table.items()):
+        for dest, current_dist in list(self.distanceVector.items()):
             # Skip direct neighbors in initial routing
             if dest == self.name:
                 continue
-
             # Check routes through each neighbor
             for neighbor_name, neighbor_info in self.neighbors.items():
                 neighbor = neighbor_info['node']
                 direct_link_dist = neighbor_info['distance']
-
                 # Bellman-Ford equation: new_distance = direct_link + neighbor's known distance to destination
-                if dest in neighbor.routing_table:
-                    new_distance = direct_link_dist + neighbor.routing_table[dest]
-                    
+                if dest in neighbor.distanceVector:
+                    new_distance = direct_link_dist + neighbor.distanceVector[dest]
                     # Update if new route is shorter
-                    if dest not in self.routing_table or new_distance < self.routing_table[dest]:
-                        self.routing_table[dest] = new_distance
+                    if dest not in self.distanceVector or new_distance < self.distanceVector[dest]:
+                        self.distanceVector[dest] = new_distance
                         updated = True
-        
         return updated
 
-    def print_routing_table(self):
-        """Print the current routing table"""
-        print(f"Routing Table for Node {self.name}:")
-        for dest, distance in self.routing_table.items():
-            print(f"To {dest}: Distance = {distance}")
-        print()
-
+    def print_distanceVector(self):
+        table = str(self.name)+": "
+        for neighbor in self.distanceVector.items():
+            table+=str(neighbor)
+        print(table)
 
 class RoutingNetwork:
     def __init__(self):
         self.nodes = {}
 
     def add_node(self, node):
-        """Add a node to the network"""
         self.nodes[node.name] = node
 
     def connect_nodes(self, node1, node2, distance):
-        """Connect two nodes with a specific distance"""
         node1.add_neighbor(node2, distance)
         node2.add_neighbor(node1, distance)
 
-    def run_routing_protocol(self, iterations=5):
-        """Simulate routing protocol updates"""
-        print("Running Distance Vector Routing Simulation\n")
-        
-        # Initial routing tables
+    def routing_protocol(self, iterations=5):       
+        print("--------------- Initial routing tables ---------------")
         for node in self.nodes.values():
-            node.print_routing_table()
-
-        # Perform routing updates
+            node.print_distanceVector()
+        # Routing updates
         for i in range(iterations):
-            print(f"\nIteration {i + 1}:")
+            print("-------------------- Iteration" , i+1, "--------------------")
             updated_nodes = []
-            
             # Collect nodes that updated their routing tables
             for node in self.nodes.values():
-                if node.update_routing_table():
+                if node.update_distanceVector():
                     updated_nodes.append(node.name)
-            
-            # Print updated routing tables
+            print("--------------- Update routing tables ---------------")
             for node_name in updated_nodes:
-                self.nodes[node_name].print_routing_table()
+                self.nodes[node_name].print_distanceVector()
 
 #MAIN
 
@@ -107,4 +90,4 @@ network.connect_nodes(B, D, 3)
 network.connect_nodes(C, D, 5)
 
 # Run routing simulation
-network.run_routing_protocol()
+network.routing_protocol(iterations)
